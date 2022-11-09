0a6d76ebd95facaa958fbef28f9063ecd4da990b
Timothy <tcuthbert@dvt.co.za>
2022-11-09 12:46:17 +0200

    + podcast

--- src/app/podcasts/podcast/podcast.component.ts
+++ src/app/podcasts/podcast/podcast.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '@base/services/api';
-import { Podcast, Podcasts } from '@base/services/data-access/model';
-import { map } from 'rxjs';
+import { Episodes, Podcast, Podcasts } from '@base/services/data-access/model';
+import { combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'base-podcast',
@@ -17,5 +17,21 @@ export class PodcastComponent {
         (Object.values(podcast) as Podcast[]).find(podcast => podcast.uuid === this.activatedRoute.snapshot.paramMap.get('uuid'))
       )
     );
-  constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService) {}
+  episode: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService) {
+    combineLatest(
+      this.apiService
+        .getAll<Podcasts>('podcasts')
+        .pipe(
+          map(podcast =>
+            (Object.values(podcast) as Podcast[]).find(podcast => podcast.uuid === this.activatedRoute.snapshot.paramMap.get('uuid'))
+          )
+        ),
+      this.apiService.getAll<Podcasts>('episodes')
+    ).subscribe(([podcast, episodes]) => {
+      this.episode = Object.values(episodes).find(episode => episode.podcastId === podcast?.id);
+      console.log("this.episode", this.episode)
+    });
+  }
 }
